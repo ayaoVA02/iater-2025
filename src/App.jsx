acf5c75b6c22ae014f2a5f5946be4b9424874b7c
unknown <ayaoVA02>
2025-10-02 10:04:52 +0700

    love

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import HomePage from "./pages/HomePage"
 import AboutPage from "./pages/AboutPage"
@@ -10,6 +10,7 @@ import AIEducationTabs from "./pages/AIEducationTabs"
 import BlogPostDetail from "./pages/BlogPostDetail"
 import Onboard from "./pages/Onboard"
 import Professors from "./pages/Professors"
+import Test from "./pages/Test"
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
         <Route path="/" element={<Onboard />} />
 
         {/* Main site inside Layout */}
-          <Route path="test" element={<HomePage />} />
+          <Route path="test" element={<Test />} />
         <Route  element={<Layout />}>
           <Route path="home" element={<HomePage />} />
           <Route path="about" element={<AboutPage />} />
