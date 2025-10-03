6397e4c268cbf8c89ac71a595d0fbd8c830c98bf
unknown <ayaoVA02>
2025-10-03 10:58:27 +0700

    h

--- src/App.jsx
+++ src/App.jsx
@@ -14,22 +14,24 @@ import Test from "./pages/Test"
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Onboard shown at root */}
-        <Route index element={<Onboard />} />
 
-        <Route path="/" element={<Layout />}>
-          <Route path="home" element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="project" element={<ProjectPage />} />
-          <Route path="program" element={<ProgramPage />} />
-          <Route path="professors" element={<Professors />} />
-          <Route path="history" element={<HistoryPage />} />
-          <Route path="aieducation" element={<AIEducationTabs />} />
-        </Route>
-      </Routes>
-    </Router>
+    // <Routes>
+    //   {/* Onboard shown at root */}
+    //   <Route index element={<Onboard />} />
+
+    //   <Route path="/" element={<Layout />}>
+    //     <Route path="home" element={<HomePage />} />
+    //     <Route path="about" element={<AboutPage />} />
+    //     <Route path="project" element={<ProjectPage />} />
+    //     <Route path="program" element={<ProgramPage />} />
+    //     <Route path="professors" element={<Professors />} />
+    //     <Route path="history" element={<HistoryPage />} />
+    //     <Route path="aieducation" element={<AIEducationTabs />} />
+    //   </Route>
+    // </Routes>
+    <div>
+      <h1>Hellow</h1>
+    </div>
   )
 }
 
